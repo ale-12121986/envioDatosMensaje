07b969dcf78a4021dc5bc68942e3a0f6e456ad21
Alejandro Anselmi <71045880+ale-12121986@users.noreply.github.com>
2023-10-19 14:22:23 -0300

    Initial commit

---
+++ EnviarMensaje.h
@@ -0,0 +1,59 @@
+#include "SoftwareSerial.h"
+#include "pins_arduino.h"
+#include "Arduino.h"
+SoftwareSerial mySerial(3, 2);
+class EnviarMensaje{
+  private:
+    int contador =0;
+    
+    //Se envian los mensajes por puerto serial al modulo sim800l
+    void updateSerial(){
+      delay(500);
+      while (Serial.available()) 
+      {
+        mySerial.write(Serial.read());//reenvía lo que ha recibido el puerto del software serial 
+      }
+      while(mySerial.available()) 
+      {
+        Serial.write(mySerial.read());//reenvía el software serial recibido al puerto serial 
+      }
+    }
+    //Se configura con conmando AT para el envio de mensaje de texto
+    void configurarDatosMensajes(){
+      String respuesta="";
+      mySerial.println("AT"); //una vez que las pruebas están hechas exitosamente, regresará un OK
+      //updateSerial();
+      while (respuesta =="OK") {
+        respuesta = mySerial.readString();
+        delay(10);
+        contador++;
+        if(contador == 50){
+          mySerial.println("AT");
+          contador = 0;
+          respuesta = "";
+        }
+      }
+      mySerial.println("AT+CMGF=1"); // Configurando modo texto
+      while (mySerial.readString() =="OK") {
+        mySerial.println("AT+CMGF=1");
+          contador = 0;
+          respuesta = "";
+      }
+      mySerial.println("AT+CMGS=\"+ZZxxxxxxxxxx\"");//cambia ZZ  código del país y xxxxxxxxxxx con el número de teléfono a sms
+      updateSerial();
+      mySerial.println("AT+CNMI=1,2,0,0,0"); // Se  configura como se van a recibir los mensajes  
+    }
+
+  public:
+    void configurar(){
+      mySerial.begin(9600);
+      configurarDatosMensajes();
+    }
+    void enviarMensajeTexto(String mensaje){
+      
+      mySerial.print(mensaje); //contenido del texto
+      updateSerial();
+      //mySerial.write(26);
+    }
+    
+};
---
+++ envioDatosMensaje.ino
@@ -0,0 +1,78 @@
+#include "EnviarMensaje.h"
+#include "SoftwareSerial.h"
+//SoftwareSerial mySerial(3, 2);
+const int fallaEncendido = 4;  // Entrada de falla de encendido
+const int senalR = 5;  // entrada de señal R
+const int senalS = 6;  // entrada de señal S
+const int senalT = 7;  // entrada de señal T
+const int encenderMotor = 8; 
+int  fallaAccion;
+int senalActiva = 0;
+int senalRAnterior = 0;
+int senalSAnterior = 0;
+int senalTAnterior = 0;
+String cadenaCharEntrada = "", fallaGenerador = "";  
+char vectorChar[20];
+bool mensajeEnviado = false, finCadena = false;
+EnviarMensaje enviarMensaje;
+void serialEvent(){
+  //Recepción de datos Seriales
+  while (Serial.available()) {              //Si existen datos seriales, leer a todos
+    char CaracterEntrada = Serial.read();   //Leer 1 byte serial recibido
+   
+    cadenaCharEntrada += CaracterEntrada;   //Agregar el nuevo char a una cadena String 
+    if (CaracterEntrada == '\n') {          //Si el char o byte recibido es un fin de linea, activa la bandera
+      finCadena = true;                        //Si la bandera finCadena = 1, entonces la transmision esta completa
+    }
+    
+  }
+  
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  
+  mySerial.begin(9600);
+  pinMode(fallaEncendido, INPUT);
+  pinMode(senalR, INPUT);
+  pinMode(senalS, INPUT);
+  pinMode(senalT, INPUT);
+  pinMode(encenderMotor, OUTPUT);
+  Serial.begin(9600);
+  enviarMensaje.configurar();
+  cadenaCharEntrada.reserve(20);
+  
+}
+// void enviarMensaje(String valor){
+
+// }
+void loop() {
+  // put your main code here, to run repeatedly: 
+  if((digitalRead(senalR))&&(senalRAnterior == 0)) {
+    senalRAnterior = 1;
+    senalSAnterior = 0;
+    senalTAnterior = 0;
+    enviarMensaje.enviarMensajeTexto("Se trabaja con la la fase R");
+  } 
+  if((digitalRead(senalS))&&(senalSAnterior == 0)) {
+    senalRAnterior = 0;
+    senalSAnterior = 1;
+    senalTAnterior = 0;
+    enviarMensaje.enviarMensajeTexto("Se trabaja con la la fase S");
+  //statements
+  } 
+  if((digitalRead(senalT))&&(senalTAnterior == 0)) {
+    senalRAnterior = 0;
+    senalSAnterior = 0;
+    senalTAnterior = 1;
+    enviarMensaje.enviarMensajeTexto("Se trabaja con la la fase T");
+  //statements
+  }
+  if(digitalRead(fallaEncendido) == HIGH){
+    if(mensajeEnviado == true){
+      mensajeEnviado = true;
+      enviarMensaje.enviarMensajeTexto("falla en el encendido");
+    }
+    
+  }
+}
